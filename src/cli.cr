d9d09c4cd169f5d3fda95079c9e172e0b1b831e1
Will Lewis <will@wflewis.com>
2019-10-22 22:13:12 -0700

    Add fullwidth

--- src/cli.cr
+++ src/cli.cr
@@ -6,6 +6,11 @@ parser = OptionParser.new do |parser|
 
   parser.on("-v", "--version", "display the version") { puts Transform::VERSION; exit 0 }
   parser.on("-h", "--help", "show this help") { puts parser; exit 0 }
+
+  parser.unknown_args { |args| ARGV.replace(args) }
 end
 
 parser.parse!
+
+input = ARGV.join(" ")
+puts Transform.fullwidth(input)
--- src/transform.cr
+++ src/transform.cr
@@ -1,3 +1,18 @@
 module Transform
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
+  extend self
+
+  def fullwidth(input)
+    String.build do |s|
+      input.each_char do |char|
+        case char.to_s
+        when .match(/[a-zA-Z]/)
+          s << char + 0xFEE0
+        else
+          s << char
+        end
+      end
+    end
+  end
 end
